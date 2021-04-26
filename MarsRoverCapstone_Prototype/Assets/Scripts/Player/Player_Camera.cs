150e5b8535f13ed3fb44c099e402a830c0f63a9c
Dallas Smart <dsmart10@outlook.com>
2021-04-26 19:54:08 +1000

    Orbiting Camera and Player movement adjustments

--- MarsRoverCapstone_Prototype/Assets/Scripts/Player/Player_Camera.cs
+++ MarsRoverCapstone_Prototype/Assets/Scripts/Player/Player_Camera.cs
@@ -6,34 +6,55 @@ public class Player_Camera : MonoBehaviour
 {
     public GameObject player => FindObjectOfType<Player_Movement>().gameObject;
 
-    public Vector3 perspectiveOffset = new Vector3 (0f, 15f, 15f);
-    public Vector3 orthoOffset = new Vector3(8f, 22f, 7f);
+    public bool orbitCamera = true;
+    public Vector3 cameraOffset = new Vector3(15f, 15f, 15f);
+
+    // Mouse controls
+    public float mouseSensitivity = 5f;  
+    [Range(0.01f, 1.0f)] public float cameraSmooth = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
- 
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (Camera.main.orthographic)
+        transform.LookAt(player.transform.position);
+      
+        switch (orbitCamera)
         {
             case true:
-                transform.position = new Vector3(player.transform.position.x + orthoOffset.x, 
-                    player.transform.position.y + orthoOffset.y, 
-                    player.transform.position.z + orthoOffset.z);
-
+                OrbitCamera();
                 break;
 
             case false:
-                transform.position = new Vector3(player.transform.position.x + perspectiveOffset.x, 
-                    player.transform.position.y + perspectiveOffset.y, 
-                    player.transform.position.z + perspectiveOffset.z);
-
+                LockedCamera();
                 break;
-
         }
+    }
+
+    // Camera that orbits position of player via mouse controls
+    private void OrbitCamera()
+    {
+        // Get mouse X input and calculate new position around player
+        float rotateHorizontal = Input.GetAxis("Mouse X");
+
+        Quaternion cameraAngle = Quaternion.AngleAxis(rotateHorizontal * mouseSensitivity, Vector3.up);
+
+        cameraOffset = cameraAngle * cameraOffset;
+
+        Vector3 cameraPosition = player.transform.position + cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, cameraPosition, cameraSmooth);
+    }
+       
+    // Camera follows player in locked rotation
+    private void LockedCamera()
+    {
+        transform.position = new Vector3(player.transform.position.x + cameraOffset.x, 
+            player.transform.position.y + cameraOffset.y, 
+            player.transform.position.z + cameraOffset.z);
     }
 }
