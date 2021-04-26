86b5e275b9f615414b5aa9db150c79266ba17f91
Dallas Smart <dsmart10@outlook.com>
2021-04-26 13:07:24 +1000

    Added Rover Jump to test scene

--- MarsRoverCapstone_Prototype/Assets/Scripts/Player/Player_Movement.cs
+++ MarsRoverCapstone_Prototype/Assets/Scripts/Player/Player_Movement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(BoxCollider))]
 
 public class Player_Movement : MonoBehaviour
 {
@@ -19,7 +20,12 @@ public class Player_Movement : MonoBehaviour
     // Speed variables, the range between min and max speed is -1 to 1
     public float driveSpeed = 0.03f;
     public float rotateSpeed = 1f;
-    public float jumpSpeed;
+    public float jumpSpeed = 400f;
+
+    Rigidbody body => GetComponent<Rigidbody>();
+    BoxCollider col => GetComponent<BoxCollider>();
+
+    public bool grounded;
 
     // Start is called before the first frame update
     void Start()
@@ -33,14 +39,29 @@ public class Player_Movement : MonoBehaviour
         // Movement inputs for WASD and Arrow keys trigger continuous translation
         float acceleration = Input.GetAxis("Vertical") * driveSpeed;
         float rotation = Input.GetAxis("Horizontal") * rotateSpeed;
+        
+        if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            body.AddForce(transform.up * jumpSpeed);
+        }
 
         transform.Translate(0, 0, acceleration);
         transform.Rotate(0, rotation, 0);
     }
 
-    // Method for jump
-    private void Jump() 
-    { 
+    private void OnCollisionEnter(Collision c)
+    {
+        if(c.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
 
+    private void OnCollisionExit(Collision c)
+    {
+        if (c.gameObject.tag == "Ground")
+        {
+            grounded = false;
+        }
     }
 }
