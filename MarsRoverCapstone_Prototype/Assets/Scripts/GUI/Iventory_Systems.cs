4142fdd249c7a05e488b604ef7ab666c4d5c8cf0
Brittany R <68683252+Brich97@users.noreply.github.com>
2021-05-10 15:01:22 +1000

    setting up inventory script

---
+++ MarsRoverCapstone_Prototype/Assets/Scripts/GUI/Iventory_Systems.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Iventory_Systems : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   void AddToInventory()
+    {
+        //adding items to the player inventory 
+    }
+
+    void ClearInventory()
+    {
+        //clear all items on the player upon 'death'
+    }
+
+    void SaveInventory()
+    {
+        //clear items but save them to a list that's accessable to player. 
+    }
+}
