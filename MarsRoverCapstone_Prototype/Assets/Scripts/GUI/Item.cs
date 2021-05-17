184a88d2bbde7e317442c8dc063efa4e06b29f1c
Brittany R <68683252+Brich97@users.noreply.github.com>
2021-05-17 20:19:02 +1000

    Moved dust hazard into dallas test scene for testing.
    Feel free to remove this.
    Also renamed Sample_Item script to Item

--- MarsRoverCapstone_Prototype/Assets/Scripts/GUI/Item.cs
+++ MarsRoverCapstone_Prototype/Assets/Scripts/GUI/Item.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Sample_Item : ScriptableObject
+public class Item : ScriptableObject
 {
     //scriptable objects exist outside of the game world
 
