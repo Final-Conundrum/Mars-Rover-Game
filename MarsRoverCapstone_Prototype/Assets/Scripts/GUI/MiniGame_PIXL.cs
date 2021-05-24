1b1f6ea04ad6a3d8276eae9d0c93cf9d360c2811
Damon Healy <39818424+ARCANGEL12@users.noreply.github.com>
2021-05-24 15:25:32 +1000

    script for minigame

--- MarsRoverCapstone_Prototype/Assets/Scripts/GUI/MiniGame_PIXL.cs
+++ MarsRoverCapstone_Prototype/Assets/Scripts/GUI/MiniGame_PIXL.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MiniGame_PIXL : MonoBehaviour
+
 {
-    //This is to load the Scene for the Mini Game for the PIXL
-    private void OnTriggerEnter(BoxCollider other)
+    public static bool Completed = false;
+    public void Exit() { Completed = true;
+        Destroy(this.gameObject); }
+// Start is called before the first frame update
+void Start()
     {
-        if (other.name == gameObject.tag)
-            if (gameObject.tag == "Player")
-                SceneManager.LoadScene("Test_MiniGame");
+     
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
 }
