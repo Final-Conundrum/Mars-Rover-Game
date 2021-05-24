d004365a7c57f0ec797f6d138e165d2921277642
Damon Healy <39818424+ARCANGEL12@users.noreply.github.com>
2021-05-24 17:53:55 +1000

    changes
    here are the changes, for some strange reason it isn't allowing me to get the right version on my end, however i promised to only touch my work and only my work nothing more, once again i do apologize for the inconvenience and that should solve the problems, if not let me know as soon as possible,
    
    Kindest Regards,
    Damon

--- MarsRoverCapstone_Prototype/Assets/Scripts/MiniGame/MiniGame_SHERLOC.cs
+++ MarsRoverCapstone_Prototype/Assets/Scripts/MiniGame/MiniGame_SHERLOC.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MiniGame_System : MonoBehaviour
+public class MiniGame_SHERLOC : MonoBehaviour
+
 {
+    public static bool Completed = false;
+    public void Exit()
+    {
+        Completed = true;
+        Destroy(this.gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
 }
