23b429709c41b60696ca862cddf547d4d96d3fd9
Dallas Smart <dsmart10@outlook.com>
2021-10-11 10:47:39 +1000

    Intro: Setting up camera transitions and button timeline

--- MarsRoverCapstone_Prototype/Assets/Scripts/IntroCameraDirector.cs
+++ MarsRoverCapstone_Prototype/Assets/Scripts/IntroCameraDirector.cs
@@ -5,28 +5,47 @@ using Cinemachine;
 
 public class IntroCameraDirector : MonoBehaviour
 {
-    public CinemachineVirtualCamera sceneCamera, goalsCamera, perseveranceCamera, instrumentCamera, minigameCamera, POICamera, hazardCamera, SZCamera, disclaimerCamera, finalCamera;
-    public GameObject[] sceneProps, goalsProps, perseveranceProps, instrumentProps, minigameProps, POIProps, hazardProps, SZProps, disclaimerProps, finalProps;
+    // Intro Director: Setup and activate different cameras and props in sequence to communicate the games goals and disclaimers
+    // Organize priority of cameras and enable props for each part of sequence
+
+    public CinemachineVirtualCamera[] sceneCameras;
+    public GameObject[] sceneProps, goalsProps, perseveranceProps, SZProps, disclaimerProps, finalProps;
+    private GameObject[][] propCollections;
+    private int currentEvent = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        propCollections = new GameObject[][] { sceneProps, goalsProps, perseveranceProps, SZProps, disclaimerProps, finalProps };
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void nextCamera(int cameraPriority, CinemachineVirtualCamera camera, GameObject[] props)
-    {
-        cameraPriority++;
-
-        foreach(GameObject i in props)
+        // Activate first sequence objects
+        foreach(GameObject i in propCollections[0])
         {
             i.SetActive(true);
         }
+
+        sceneCameras[0].Priority = 11;
+    }
+
+    public void GoToCamera(int cameraPriority)
+    {
+        // Deactivate previous objects
+        foreach(GameObject i in propCollections[currentEvent])
+        {
+            i.SetActive(false);
+        }
+        sceneCameras[currentEvent].Priority = 1;
+
+        // Camera transition
+        currentEvent = cameraPriority;
+        sceneCameras[cameraPriority].Priority = 11;
+
+        // Activate next objects
+        foreach (GameObject i in propCollections[currentEvent])
+        {
+            i.SetActive(true);
+        }
+
+        Debug.Log("Intro Camera: Current camera priority = sceneCamera[" + cameraPriority + "]");
     }
 }
